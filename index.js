219dd55af0b94b9dcb53e86fd8bb4b3481ed564b
Piotr Proszowski <60212583+Ptreee@users.noreply.github.com>
2021-12-02 09:36:47 +0100

    add debounce function to input, create utils

---
+++ index.js
@@ -0,0 +1,16 @@
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: '2bd626ff',
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const onInput = (e) => {
+  fetchData(e.target.value);
+};
+input.addEventListener('input', debounce(onInput, 500));
