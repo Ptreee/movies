3e4d7d06d179f938a23b5e834c15edcd192a91fb
Piotr Proszowski <60212583+Ptreee@users.noreply.github.com>
2021-12-02 15:35:55 +0100

    extract rendering logic

--- index.js
+++ index.js
@@ -12,54 +12,15 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-<div class="dropdown">
-  <div class="dropdown-menu">
-    <div class="dropdown-content results"></div>
-  </div>
-</div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (e) => {
-  const movies = await fetchData(e.target.value);
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-
-  for (const movie of movies) {
-    const option = document.createElement('a');
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-
-    option.innerHTML = `
+    return `
     <img src="${imgSrc}" />
     ${movie.Title} (${movie.Year})
     `;
-    option.addEventListener('click', () => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-    resultsWrapper.appendChild(option);
-  }
-};
-input.addEventListener('input', debounce(onInput, 1000));
-
-document.addEventListener('click', (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
+  },
 });
 
 const onMovieSelect = async (movie) => {
