abaa601d39ad29f99bb3d4430b0eea387b5563bb
Piotr Proszowski <60212583+Ptreee@users.noreply.github.com>
2021-12-03 11:20:46 +0100

    add comparison styles

--- index.js
+++ index.js
@@ -66,7 +66,27 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-  console.log(`time for comparison`);
+  const leftSideStats = document.querySelectorAll(
+    '#left-summary .notification'
+  );
+  const rightSideStats = document.querySelectorAll(
+    '#right-summary .notification'
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove('is-primary');
+      leftStat.classList.add('is-warning');
+    } else {
+      rightStat.classList.remove('is-primary');
+      rightStat.classList.add('is-warning');
+    }
+  });
 };
 
 const movieTemplate = (movieDetail) => {
@@ -85,7 +105,6 @@ const movieTemplate = (movieDetail) => {
       return prev + value;
     }
   }, 0);
-  // console.log(awards);
 
   return `
   <article class="media">
